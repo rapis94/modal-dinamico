bed80e55eed2a9a5cc200e44e7557c8e31f7409f
rapis94 <68077986+rapis94@users.noreply.github.com>
2024-05-28 00:05:58 -0300

    Create modal.php

---
+++ modal.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="modal" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="modal-label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background: var(--bs-danger)">
+                <h1 class="modal-title fs-5" id="modal-label">
+                    
+                </h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style="color: var(--fuente2)" id="modal-body">
+
+            </div>
+            <div class="modal-footer" id="modal-botonera">
+                
+            </div>
+        </div>
+    </div>
+</div>
